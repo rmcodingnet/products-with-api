74e0d78737be752a9fd439875e4e9d7115402451
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-11 17:03:17 +0000

    Added navbar with unfinalised styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar">
+            <div className="navItem">
+                <Link to="/">Product List</Link>
+            </div>
+            <div className="navItem">
+                <Link to="/addProduct">Add New Product</Link>
+            </div>
+            <div className="navItem">
+                <Link to="/checkout">Basket</Link>
+            </div>
         </div>
     );
 };

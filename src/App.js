7e8711f97c45c6aaa79939737e777d80624c40a9
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-11 17:01:44 +0000

    Added initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList';
+import UpsertProduct from './components/UpsertProduct/UpsertProduct';
+import Basket from './components/Basket/Basket';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Switch>
+        <Route
+          exact={true}
+          path="/addProduct"
+          render={(props) => (
+            <UpsertProduct />
+          )}
+        />
+        <Route
+          exact={true}
+          path="/checkout"
+          render={(props) => (
+            <Basket />
+          )}
+        />
+        <Route
+          exact={true}
+          path="/"
+          render={(props) => (
+            <ProductList />
+          )}
+        />
+      </Switch>
     </div>
   );
 }

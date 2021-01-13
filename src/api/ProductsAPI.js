ec76bd7630b0b5a8ffaa53f20ba14f9055b9ac6f
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-13 11:23:02 +0000

    Added addProduct api call

--- src/api/ProductsAPI.js
+++ src/api/ProductsAPI.js
@@ -1,8 +1,16 @@
 export const getList = () => {
-    return  fetch("http://localhost:8000/api/products").then(response => response.json())
+    return fetch("http://localhost:8000/api/products").then(response => response.json())
 }
 
-
+export const addProduct = (product) => {
+    return fetch("http://localhost:8000/api/products", {
+        method: 'post',
+        body: JSON.stringify(product),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json());
+}
 
 
 

99a1bf4bc6afa9f5fcd6b947ba047e8b1cf55c0c
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-20 16:33:15 +0000

    added html validation & moved api call functions

--- src/components/UpsertProduct/UpsertProduct.js
+++ src/components/UpsertProduct/UpsertProduct.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { withRouter, useParams } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
+import { withRouter, useParams, Redirect } from 'react-router-dom';
 import moment from 'moment';
+import { addProduct, updateProduct } from '../../api/ProductsAPI';
 import './UpsertProduct.css'
 
-const UpsertProduct = ({addProduct, updateProduct, history, products}) => {
+const UpsertProduct = ({ products, refresh }) => {
     const [values, setValues] = useState({});
+    const [redirect, setRedirect] = useState(false);
+    const [formErrors, setFormErrors] = useState({})
 
     const { productID } = useParams();
-    
+
 
     const product = products.length > 0 && products.find(item => item.id === parseInt(productID));
 
     useEffect(() => {
-        if(product) {
-            setValues({...product})
+        if (product) {
+            setValues({ ...product, expiryDate: moment(product.expiryDate).format('YYYY-MM-DD') })
         }
-        
-    },[product])
+
+    }, [product])
+
+
+    const addNewProduct = (product) => {
+        addProduct(product).then(result => {
+            if (result.newID) {
+                alert("New Product Added");
+                refresh();
+                setRedirect(true);
+            } else {
+                return alert("Error adding new product")
+            }
+        });
+    }
+
+
+    const updateExistingProduct = (id, product) => {
+        updateProduct(id, product).then(result => {
+            if(result.ok) {
+                alert("Product Updated")
+                refresh();
+                setRedirect(true);
+            }
+            return result.ok ? alert("Product Updated") : alert("An Error Occured");
+        });
+    }
+
 
     const handleChangeValues = (newValue) => {
         setValues({ ...values, ...newValue });
@@ -30,37 +58,41 @@ const UpsertProduct = ({addProduct, updateProduct, history, products}) => {
             name: values.name,
             type: values.type,
             price: values.price,
-            expiryDate: values.expiryDate, 
-            description: values.description, 
+            expiryDate: values.expiryDate,
+            description: values.description,
             country: values.country
         }
         console.log(newProduct)
-        product ? updateProduct(product.id,newProduct) : addProduct(newProduct);
-        history.push('/');
+        product ? updateExistingProduct(product.id, newProduct) : addNewProduct(newProduct);
+    }
+
+    if (redirect) {
+        return <Redirect to=""></Redirect>
     }
 
     return (
         <div className="form-container">
             <form className="form" onSubmit={(e) => handleSubmit(e)}>
                 <label>Name</label>
-                <input required type="text" value={values.name || ""} onChange={(e) => handleChangeValues({ name : e.target.value})}/>
-                <br/>
+                <input required type="text" value={values.name || ""} onChange={(e) => handleChangeValues({ name: e.target.value })} />
+                <br />
                 <label>Material</label>
-                <input required type="text" value={values.type || ""} onChange={(e) => handleChangeValues({ type : e.target.value})}/>
-                <br/>
+                <input required type="text" value={values.type || ""} onChange={(e) => handleChangeValues({ type: e.target.value })} />
+                <br />
                 <label>Price (£)</label>
-                <input required type="number" value={values.price || ""} onChange={(e) => handleChangeValues({ price : e.target.value})}/>
-                <br/>
+                <input required type="number" value={values.price || ""} onChange={(e) => handleChangeValues({ price: e.target.value })} />
+                <br />
                 <label>Expiry Date</label>
-                <DatePicker selected={values.expiryDate !== "" ? moment(values.expiryDate).toDate() : moment().toDate()} onChange={date => handleChangeValues({ expiryDate: moment(date).format('YYYY-MM-DDTHH:mm:ss[Z]') })} />
-                <br/>
+                <input required type="date" value={values.expiryDate || ""} onChange={(e) => handleChangeValues({ expiryDate: e.target.value })} />
+                {/* <DatePicker selected={values.expiryDate !== "" ? moment(values.expiryDate).toDate() : moment().toDate()} onChange={date => handleChangeValues({ expiryDate: moment(date).format('YYYY-MM-DDTHH:mm:ss[Z]') })} /> */}
+                <br />
                 <label>Description</label>
-                <textarea required type="text" value={values.description || ""} onChange={(e) => handleChangeValues({ description : e.target.value})}/>
-                <br/>
+                <textarea required type="text" value={values.description || ""} onChange={(e) => handleChangeValues({ description: e.target.value })} />
+                <br />
                 <label>Country</label>
-                <input required type="text" value={values.country || ""} onChange={(e) => handleChangeValues({ country : e.target.value})}></input>
-                <br/>
-                {product ? <button type="submitBtn">Update Product</button> : <button type="submit" className="submitBtn">Add Product</button> }
+                <input required type="text" value={values.country || ""} onChange={(e) => handleChangeValues({ country: e.target.value })}></input>
+                <br />
+                {product ? <button type="submitBtn">Update Product</button> : <button type="submit" className="submitBtn">Add Product</button>}
             </form>
         </div>
     );
